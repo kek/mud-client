304f8f0045210a262a5cc7801e7a81a9520a35ca
Karl Eklund <localpart@gmail.com>
2019-01-13 22:15:34 +0100

    MudTest

--- test/mud_test.exs
+++ test/mud_test.exs
@@ -1,11 +1,20 @@
 defmodule MudTest do
   use ExUnit.Case
   doctest Mud
+  alias Mud.{TestServer}
 
   test "greets the world" do
-    Mud.TestServer.start_link(5001)
+    TestServer.start_link(5001)
     {:ok, socket} = :gen_tcp.connect('localhost', 5001, [])
     IO.inspect(socket, label: "socket")
     :ok = :gen_tcp.send(socket, 'BU!\r')
+
+    receive do
+      msg -> IO.inspect(msg, label: "got")
+    after
+      1000 -> true
+    end
+
+    :ok = :gen_tcp.close(socket)
   end
 end
--- test/support/test_server.ex
+++ test/support/test_server.ex
@@ -1,14 +1,18 @@
 defmodule Mud.TestServer do
   alias Mud.Conversation
 
+  defmodule State do
+    defstruct nothing: nil
+  end
+
   def init(port) do
     {:ok, listening_socket} = :gen_tcp.listen(port, [])
     IO.puts("listening #{inspect(listening_socket)}")
 
-    {:ok, [], {:continue, listening_socket}}
+    {:ok, %State{}, {:continue, listening_socket}}
   end
 
-  def handle_continue(listening_socket, state) do
+  def handle_continue(listening_socket, state = %State{}) do
     {:ok, socket} = :gen_tcp.accept(listening_socket)
     IO.puts("connected #{inspect(socket)}")
     :ok = :gen_tcp.controlling_process(socket, Conversation.start_link())
